2494ce0d03e5bc7eced53dc224bcd87d92e3354d
Ankit Somani <ankit.m.somani@gmail.com>
2021-02-04 16:14:10 -0500

    prompts added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword(){
+var passwordLength = prompt("Choose A Password Length Between 8 & 128")
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  confirm("Choose A Character Type")
+} 
 
 // Write password to the #password input
 function writePassword() {
