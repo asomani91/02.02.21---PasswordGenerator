d2cf937e6c81dc156d1e6da39ac1cd5f1d05b4ff
Ankit Somani <ankit.m.somani@gmail.com>
2021-02-04 21:57:24 -0500

    var additions

--- script.js
+++ script.js
@@ -5,17 +5,24 @@ var number = "0123456789"
 var uppercaseLetter = "abcdefghijklmnopqrstuvwxyz"  
 var lowercaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var specialCharacter =  "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var password = [number, uppercaseLetter, lowercaseLetter, specialCharacter]
 
-function generatePassword() {
-  var confirmPasswordLength = prompt("Choose a password length between 8 & 128");
-  console.log(confirmPasswordLength);
+function writePassword() {
+  var confirmPasswordLength = prompt("Choose a password length between 8 & 128")
+  console.log(typeof parseInt(confirmPasswordLength));
 
-  var confirmUpperCase = prompt("Would you like to include uppercase letters?");
+  var confirmNumber = confirm("Would like to include a number?")
+  console.log(confirmNumber);
+
+  var confirmUpperCase = confirm("Would you like to include uppercase letters?")
   console.log(confirmUpperCase);
 
+  var confirmLowerCase = confirm("Would you like to inlude lowercase letter?")
+  console.log(confirmLowerCase);
 
-  
-}
+  var confirmSpecialCharacter = confirm("Would you like special characters?")
+  console.log(confirmSpecialCharacter);
+};
 
 // Write password to the #password input
 function writePassword() {
